ab4d9b65f166e6d9e2f44a42309d1832af3bb517
Donovan McCray <donovanmccray@wireless-10-145-206-123.public.utexas.edu>
2019-09-16 19:17:37 -0500

    Working on getting latitude/ longitude

--- app/src/main/java/com/example/hw01weatherapp/MainActivity.java
+++ app/src/main/java/com/example/hw01weatherapp/MainActivity.java
@@ -3,13 +3,15 @@ package com.example.hw01weatherapp;
 import android.os.Bundle;
 
 import com.android.volley.Request;
+import com.android.volley.Response;
 import com.android.volley.RequestQueue;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
-import com.android.volley.Response.Listener;
-import com.android.volley.Response.ErrorListener;
-import com.google.android.gms.common.api.Response;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.MapView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
@@ -20,8 +22,13 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
+
+    private GoogleMap googleMap;
 
     public void searchLocation(View view) {
         // Get the text view
@@ -34,23 +41,64 @@ public class MainActivity extends AppCompatActivity {
         RequestQueue queue = Volley.newRequestQueue(this);
         String url ="https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=AIzaSyBHw3QHkohfaXixBI1D1n3wSk5-i02ie98\n";
 
-        // Request a string response from the provided URL.
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        // Display the first 500 characters of the response string.
-//                        textView.setText("Response is: "+ response.substring(0,500));
-                    }
-                }, new Response.ErrorListener() {
+//        // Request a string response from the provided URL.
+//        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+//                new Response.Listener<String>() {
+//                    @Override
+//                    public void onResponse(String response) {
+//                        // Display the first 500 characters of the response string.
+//
+//                        // Get the text view
+//                        TextView temperatureTextView = (TextView) findViewById(R.id.tempField);
+//                        temperatureTextView.setText("Response is: "+ response.substring(0,500));
+//
+//                        // Update MapView
+//                        MapView mapView = (MapView) findViewById(R.id.mapView);
+//                    }
+//                }, new Response.ErrorListener() {
+//            @Override
+//            public void onErrorResponse(VolleyError error) {
+////                textView.setText("That didn't work!");
+//            }
+//        });
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
+            @Override
+            public void onResponse(JSONObject response) {
+                // Get the text view
+                TextView temperatureTextView = (TextView) findViewById(R.id.tempField);
+
+                try {
+                    JSONObject results = (JSONObject) response.getJSONArray("results").get(0);
+                    JSONObject geometry = (JSONObject) results.get("geometry");
+                    JSONObject location = (JSONObject) geometry.get("location");
+
+                    String test = geometry.toString();
+                    String latitude = location.get("lat").toString();
+                    String longitude = location.get("lng").toString();
+
+                    temperatureTextView.setText("Response: " + test);
+
+                } catch(Exception e) {
+
+                }
+
+                // Update MapView
+                MapView mapView = (MapView) findViewById(R.id.mapView);
+            }
+        }, new Response.ErrorListener() {
+
             @Override
             public void onErrorResponse(VolleyError error) {
-//                textView.setText("That didn't work!");
+                // TODO: Handle error
+
             }
         });
 
         // Add the request to the RequestQueue.
-        queue.add(stringRequest);
+        queue.add(jsonObjectRequest);
     }
 
     @Override
@@ -68,6 +116,11 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        // Update MapView
+        MapView mapView = (MapView) findViewById(R.id.mapView);
+
+
     }
 
     @Override
